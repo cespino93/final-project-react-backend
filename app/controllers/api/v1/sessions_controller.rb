35635bf0286ae2cbee549e4bad135ca0787c667a
cespino93 <chrisl.espino@gmail.com>
2022-03-15 19:47:05 -0400

    finished adding logout in sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,33 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-    end
+        @user = User.find_by(username: params[:session][:username])
 
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {
+                error: "Invalid Credentials"
+            }
+    end
+end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user
+        else
+            render json: {
+                error: "Not logged in"
+            }
+        end
+     end
+
+    def destroy
+        session.clear
+        render json: {
+            notice: "logged out"
+
+        }, status: :ok
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   post "/api/v1/login", to: "api/v1/sessions#create"
+  delete "/api/v1/logout", to: "api/v1/sessions#destroy"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   # I want my api to be example.com/api/v1/resource
   namespace :api do
     namespace :v1 do
